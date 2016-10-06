c9590235de0068d2464163a1e731ccafd85b0a0a
Andrew Shaffer <andrew@weo.io>
2016-10-06 13:00:53 -0700

    add vendor prefix support

--- test/index.js
+++ test/index.js
@@ -12,5 +12,16 @@ var test = require('tape')
 test('should work', function (t) {
   t.equal(unitless.zIndex, true)
   t.equal(unitless['z-index'], true)
+  t.equal(unitless.order, true)
+
+  t.equal(unitless.webkitOrder, true)
+  t.equal(unitless['-webkit-order'], true)
+
+  t.equal(unitless.mozOrder, true)
+  t.equal(unitless['-moz-order'], true)
+
+  t.equal(unitless.msOrder, true)
+  t.equal(unitless['-ms-order'], true)
+
   t.end()
 })
